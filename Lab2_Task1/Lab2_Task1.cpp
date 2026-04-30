509986fd1eb20a6666808a37f04ff6d84576d2cb
Жарков Олександр <o.zharkov@istu.edu.ua>
2026-04-30 23:06:29 +0300

    Add files via upload

---
+++ Lab2_Task1/Lab2_Task1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x, y;
+
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "\n--- Intermediate values ---\n";
+    cout << "x = " << x << endl;
+    cout << "|x| = " << fabs(x) << endl;
+
+    if (x >= 1)
+    {
+        double expArg = -4;
+        y = exp(expArg);
+
+        cout << "Branch: x >= 1\n";
+        cout << "expArg = " << expArg << endl;
+    }
+    else if (fabs(x) < 1)
+    {
+        double inside = 1 - x * x;
+
+        cout << "Branch: |x| < 1\n";
+        cout << "inside = 1 - x^2 = " << inside << endl;
+
+        if (inside <= 0)
+        {
+            cout << "Error: sqrt(inside) is undefined (inside <= 0)\n";
+            return 0;
+        }
+
+        double root = sqrt(inside);
+        y = log10(root);
+
+        cout << "sqrt(inside) = " << root << endl;
+    }
+    else
+    {
+        y = atan(x);
+
+        cout << "Branch: x <= -1\n";
+    }
+
+    cout << "\nResult:\n";
+    cout << "y = " << y << endl;
+
+    return 0;
+}
